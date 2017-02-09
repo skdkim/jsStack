32e421b90073c34157a54b5cd226e4b94c771fa7
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-02-08 23:24:02 -0800

    create a stack data structure

--- stack.js
+++ stack.js
@@ -7,6 +7,47 @@ class StackNode{
 
 class Stack{
   constructor(){
-    let top = new StackNode();
+    this.top = new StackNode();
+  }
+
+  push(ele){
+    let item = new StackNode(ele);
+    item.next = this.top;
+    this.top = item;
+  }
+
+  pop(){
+    let popped = this.top.value;
+    this.top = this.top.next;
+
+    return popped;
+  }
+
+  peek(){
+    if (this.top.value){
+      return this.top;
+    } else {
+      return "stack is empty";
+    }
+  }
+
+  isEmpty(){
+    return (!this.top.value);
   }
 }
+
+let stack = new Stack();
+
+console.log(stack.isEmpty());
+
+//
+// // console.log(stack.peek());
+// stack.push(1);
+// stack.push(5);
+// stack.push(3);
+// stack.push(7);
+//
+// // console.log(stack.peek());
+//
+// stack.pop();
+// console.log(stack.peek());
